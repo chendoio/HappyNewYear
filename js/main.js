aa206f350b37bae947a0fd5975ef5daf84fc1fba
chendo <chendoec@live.com>
2021-12-31 15:13:34 -0500

    Happy New Year By Chendo

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const navBar = document.querySelector('.navigation');
+const menuBar = document.querySelector('.menuBar');
+const menuBarOn = document.querySelector('.menuBarOn');
+
+menuBar.addEventListener('click', open);
+
+function open() {
+    if(navBar.classList.contains('open')){
+        navBar.classList.remove('open');
+        menuBarOn.setAttribute('src', 'img/menuBarOn.svg');
+    } else {
+        navBar.classList.add('open');
+        setTimeout(() => {
+            menuBarOn.setAttribute('src', 'img/menuBar.svg');
+        }, 150);
+    }
+}
+
+   
+    
+
